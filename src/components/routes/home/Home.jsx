d6354e01abb14414376dd3b33ff7f275a767fa1d
Charday Neal <charday.neal@gmail.com>
2025-01-27 19:04:16 -0800

    Update header

--- src/components/routes/home/Home.jsx
+++ src/components/routes/home/Home.jsx
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="home grid-container">
         <Sidebar/>
-        <div className="grid-item header">HEADER</div>
+        <div className="grid-item header">
+          <h1>jobHunt</h1>
+          <p>Hello, User!</p>
+          <IconButton aria-label="log out" size="large" className="logout" >
+            <LogoutIcon /> <p>Logout</p>
+          </IconButton>
+        </div>
         <Outlet></Outlet>
       </div>
   )
