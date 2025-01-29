606e4d0d81a420f86ca9234221973e9bd409ed5c
Charday Neal <charday.neal@gmail.com>
2025-01-28 20:54:32 -0800

    Update lead search header and grid

--- src/components/routes/leadSearch/LeadSearch.jsx
+++ src/components/routes/leadSearch/LeadSearch.jsx
@@ -1,11 +1,19 @@
+import MuseHeader from "./MuseHeader";
+
+import './LeadSearch.css';
+
 // This component will be used to search for leads
 
 const LeadSearch = () => {
-    return (
-      <div className="grid-item flex-area">
-      
+  return (
+    <div className="grid-item flex-area lead-search">
+      <div className="filter-panel"></div>
+      <MuseHeader/>
+      <div className="search-container">
+        <h2>Search for leads</h2>
       </div>
-    )
+    </div>
+  )
   }
   
   export default LeadSearch;
---
+++ src/components/routes/leadSearch/MuseHeader.jsx
@@ -0,0 +1,59 @@
+import { Box, AppBar, Toolbar, Typography, TextField, InputAdornment } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const MuseHeader = () => {
+  return (
+    <Box component={'section'} className="muse-header">
+      <AppBar position="static" className="muse-app-bar">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            BROWSE MUSE FOR LEADS
+          </Typography>
+          <TextField
+            id="input-with-icon-textfield"
+            className='muse-search-bar'
+            placeholder="Search by Title or company..."
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+                inputProps: {
+                  'aria-label': 'search leads',
+                  // onInput: handleQueryChange,
+                  // value: searchQuery
+                }
+              },
+              
+            }}
+            variant="standard"
+          />
+          <div>
+            <Typography variant="span"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}>
+              API POWERED BY OUR FRIENDS AT
+            </Typography>
+            <Typography variant="span"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}>
+              THE MUSE
+            </Typography>
+          </div>
+          </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+export default MuseHeader
