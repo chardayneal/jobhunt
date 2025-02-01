2f5ce30ae86c72f80bee9291f88a651bb4f8ea82
Charday Neal <charday.neal@gmail.com>
2025-02-01 12:20:31 -0800

    Create insights page and style grid

--- src/components/routes/insights/Insights.jsx
+++ src/components/routes/insights/Insights.jsx
@@ -1,7 +1,11 @@
+import './Insights.css';
+
 const Insights = () => {
     return (
-      <div className="grid-item flex-area">
-        
+      <div className="grid-item flex-area insight-grid">
+        <div className="insight-grid-item stats-box"></div>
+        <div className="insight-grid-item timeline-view"></div>
+        <div className="insight-grid-item insight-view"></div>
       </div>
     )
   }
