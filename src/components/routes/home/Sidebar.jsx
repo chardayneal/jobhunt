f49c0b6c2dfef58bd1ac2f5960ebd291057e53dd
Charday Neal <charday.neal@gmail.com>
2025-02-11 11:24:16 -0800

    Update sidebar styling and font

--- src/components/routes/home/Sidebar.jsx
+++ src/components/routes/home/Sidebar.jsx
@@ -2,16 +2,31 @@ import { Link } from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import InsightsIcon from '@mui/icons-material/Insights';
+import lightLogo from '../../../assets/jobHuntLight.svg';
 import './Sidebar.css';
 
 const Sidebar = () => {
   return (
     <div className="grid-item sidebar">
-      <img src="/src/assets/logoDark.svg" alt="jobHunt logo" className="logo" />
+      <div className="logo"> <img src={lightLogo} alt="jobHunt logo"  /></div>
       <nav>
-        <Link to="/dashboard">{<HomeIcon/>}</Link>
-        <Link to="/dashboard/search">{<ManageSearchIcon/>}</Link>
-        <Link to="/dashboard/insights">{<InsightsIcon/>}</Link>
+        <Link to="/dashboard" className="link-comp">
+          <div className="sidebar-link">
+            <HomeIcon/>
+            <p>Dashboard</p>
+          </div>
+        </Link>
+        <Link to="/dashboard/search" className="link-comp">
+          <div className="sidebar-link">
+            <ManageSearchIcon/>
+            <p>Search</p>
+          </div></Link>
+        <Link to="/dashboard/insights" className="link-comp">
+          <div className="sidebar-link">
+            <InsightsIcon/>
+            <p>Insights</p>
+          </div>
+        </Link>
       </nav>
     </div>
   )
