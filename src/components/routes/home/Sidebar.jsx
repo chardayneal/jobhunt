43baf47120ca9d15b377946617f02ecddaff1156
Charday Neal <charday.neal@gmail.com>
2025-02-11 14:24:52 -0800

    Update active navlinks in sidebar

--- src/components/routes/home/Sidebar.jsx
+++ src/components/routes/home/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import InsightsIcon from '@mui/icons-material/Insights';
@@ -6,27 +6,33 @@ import lightLogo from '../../../assets/jobHuntLight.svg';
 import './Sidebar.css';
 
 const Sidebar = () => {
+
   return (
     <div className="grid-item sidebar">
       <div className="logo"> <img src={lightLogo} alt="jobHunt logo"  /></div>
       <nav>
-        <Link to="/dashboard" className="link-comp">
+        <NavLink 
+          to="/dashboard" 
+          className="link-comp"
+          end
+          >
           <div className="sidebar-link">
             <HomeIcon/>
             <p>Dashboard</p>
           </div>
-        </Link>
-        <Link to="/dashboard/search" className="link-comp">
+        </NavLink>
+        <NavLink to="/dashboard/search" className="link-comp" end>
           <div className="sidebar-link">
             <ManageSearchIcon/>
             <p>Search</p>
-          </div></Link>
-        <Link to="/dashboard/insights" className="link-comp">
+          </div>
+        </NavLink>
+        <NavLink to="/dashboard/insights" className="link-comp" end>
           <div className="sidebar-link">
             <InsightsIcon/>
             <p>Insights</p>
           </div>
-        </Link>
+        </NavLink>
       </nav>
     </div>
   )
